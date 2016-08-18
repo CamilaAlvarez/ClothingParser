5dede3b521ab6d245b1c08d96ba0a9f17c67a94c
Camila Alvarez <cam.alvarez.i@gmail.com>
2016-08-18 13:07:10 -0300

    Modified script to include associated product

--- WhereToBuyItEvaluation/scripts/retrieval_testing_id.sh
+++ WhereToBuyItEvaluation/scripts/retrieval_testing_id.sh
@@ -14,8 +14,9 @@ do
 	OUTPUT=$(echo $line |  awk -F '/' '{print $5}')
 	CLASS=$(echo $line | awk -F '/' '{print $6}')
 	ID=$(echo $line | awk '{print $1}')
+	PRODUCT=$(echo $line | awk -F '?' '{print $2}')
 	echo $line
-	LINE="$ID\t$CLASS"
+	LINE="$ID\t$CLASS\t$PRODUCT"
 	if [ $OUTPUT = "retrieval" ]
 	then
 		echo -e $LINE >> "retrieval.txt"
