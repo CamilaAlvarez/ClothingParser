114fad0d8fcbb2e72bb169f16f93b0a6ec4cda59
Camila Alvarez <cam.alvarez.i@gmail.com>
2016-10-18 11:23:22 -0300

    dataset creator from json

---
+++ DatasetCreator/datasetJsonParser.py
@@ -0,0 +1,16 @@
+import json
+import os
+from shutil import copyfile
+
+def createCorrectDataset(jsonLocation, baseImageLocation, baseNewDataset):
+    dataset = json.load(jsonLocation)
+    for _, value in dataset.iteritems():
+        for box in value:
+            category = box["category"]
+            filename = box["filename"]
+            categoryDirectory = baseNewDataset+category+"/"
+            if not os.path.exists(categoryDirectory):
+                os.makedirs(categoryDirectory)
+            copyfile(baseImageLocation+filename, categoryDirectory+filename)
+
+createCorrectDataset("/home/calvarez/outdoorImages.json", "/home/calvarez/Datasets/Street2Shop/Images/", "/home/calvarez/ClothinParsing")
