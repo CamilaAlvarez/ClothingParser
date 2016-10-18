e201b3f64725e6c8b786581554c5502064ff2b2b
Camila Alvarez <cam.alvarez.i@gmail.com>
2016-10-18 11:39:04 -0300

    Added print

--- DatasetCreator/datasetJsonParser.py
+++ DatasetCreator/datasetJsonParser.py
@@ -12,6 +12,7 @@ def createCorrectDataset(jsonLocation, baseImageLocation, baseNewDataset):
                 categoryDirectory = baseNewDataset+category+"/"
                 if not os.path.exists(categoryDirectory):
                     os.makedirs(categoryDirectory)
+                print "Copy file: "+filename+" of category: "+category
                 copyfile(baseImageLocation+filename, categoryDirectory+filename)
 
 createCorrectDataset("/home/calvarez/outdoorImages.json", "/home/calvarez/Datasets/Street2Shop/Images/", "/home/calvarez/ClothinParsing/")
