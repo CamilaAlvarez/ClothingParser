a0589bf340e98b30ced3cae214a4c28032c7775f
Camila Alvarez <cam.alvarez.i@gmail.com>
2016-10-18 11:36:08 -0300

    Open json file before parsing it

--- DatasetCreator/datasetJsonParser.py
+++ DatasetCreator/datasetJsonParser.py
@@ -3,14 +3,15 @@ import os
 from shutil import copyfile
 
 def createCorrectDataset(jsonLocation, baseImageLocation, baseNewDataset):
-    dataset = json.load(jsonLocation)
-    for _, value in dataset.iteritems():
-        for box in value:
-            category = box["category"]
-            filename = box["filename"]
-            categoryDirectory = baseNewDataset+category+"/"
-            if not os.path.exists(categoryDirectory):
-                os.makedirs(categoryDirectory)
-            copyfile(baseImageLocation+filename, categoryDirectory+filename)
+    with open(jsonLocation,'r') as jsonFile:
+        dataset = json.load(jsonFile)
+        for _, value in dataset.iteritems():
+            for box in value:
+                category = box["category"]
+                filename = box["filename"]
+                categoryDirectory = baseNewDataset+category+"/"
+                if not os.path.exists(categoryDirectory):
+                    os.makedirs(categoryDirectory)
+                copyfile(baseImageLocation+filename, categoryDirectory+filename)
 
 createCorrectDataset("/home/calvarez/outdoorImages.json", "/home/calvarez/Datasets/Street2Shop/Images/", "/home/calvarez/ClothinParsing/")
